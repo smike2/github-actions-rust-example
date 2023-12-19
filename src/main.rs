feee0d07b01c5139013b63baca5332695dd550e3
smike <124471564+smike2@users.noreply.github.com>
2023-12-19 07:14:02 +0000

    ready for ci

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::Parser;
+
+use trust::add;
+use trust::div;
+use trust::mul;
+use trust::sub;
+
+
 fn main() {
     println!("Hello, world!");
+    println!("add(2, 3) = {}", add(2, 3));
+    println!("sub(5, 3) = {}", sub(5, 3));
+    println!("mul(2, 3) = {}", mul(2, 3));
+    println!("div(6, 3) = {}", div(6, 3));
 }
